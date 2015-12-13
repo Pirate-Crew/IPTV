25554c183abee8c96f025ed972f30c0830ff51ca
Ludo237 <ludovico1990@hotmail.it>
2015-12-13 21:08:02 +0100

    Improve on Core
    * Added better spacing
    * Changed single quote to double quotes for strings
    * Translate the script
    * Improved Shell menu

--- IPTV/iptv.py
+++ IPTV/iptv.py
@@ -23,63 +23,62 @@
 #  are not responible for any missue or damage caused by thi program    #
 #########################################################################
 
-#by Pinperepette - The Pirate 
+#by Pinperepette - The Pirate
 
 import urllib2, google, time, pyprind ,sys
 from urlparse import urlparse
 from sys import argv as s
 
 #parametri editabili
-ricerca = 'Xtream Codes v1.0.59.5'
-lista = 'nomi_italiani.txt'
+search = "Xtream Codes v1.0.59.5"
+names = "names.txt"
 
- 
+
 def print_link():
-    for url in google.search(ricerca, num=30, stop=1):
+    for url in google.search(search, num = 30, stop = 1):
         parsed = urlparse(url)
-        print(parsed.scheme + '://' + parsed.netloc +"\n")
+        print(parsed.scheme + "://" + parsed.netloc + "\n")
 
 
 def search_account(URL):
-    righe = open( lista ,'r')
-    NR = len(righe.readlines())
+    rows = open(names , "r")
+    NR = len(rows.readlines())
     TT = (str(NR))
     n = NR
-    bar = pyprind.ProgBar(n, title=' ', stream=1, monitor=True)
+    bar = pyprind.ProgBar(n, title = " ", stream = 1, monitor = True)
     tr = 0
-    with open(lista) as f:
+    with open(names) as f:
         content = f.readlines()
     for r in content:
-        req = urllib2.Request( URL + '/get.php?username=%s&password=%s&type=m3u&output=mpegts'%(r.rstrip().lstrip(),r.rstrip().lstrip()))
+        req = urllib2.Request( URL + "/get.php?username=%s&password=%s&type=m3u&output=mpegts" % (r.rstrip().lstrip(), r.rstrip().lstrip()))
         response = urllib2.urlopen(req)
         the_page = response.read()
-
         NR = (NR - 1)
         TM = (str(NR))
-
         bar.update()
         if len(the_page) > 0:
-            tr = (tr +1)
-            out_file = open("m3u/tv_channels_%s.m3u"%r.rstrip().lstrip(),"w")
+            tr = (tr + 1)
+            out_file = open("m3u/tv_channels_%s.m3u" % r.rstrip().lstrip(), "w")
             out_file.write(the_page)
             out_file.close()
     trov = (str(tr))
-    print ('account trovati : ' + trov)
+    print ("Accounts found: " + trov)
 
 def usage():
-    
-    print'##### USAGE #####'
-    print ("for print list server " + s[0] + " " + "-pl")
-    print ("for search account " + s[0] + " " + "http://site.server")
- 
+
+    print "##### IPTV"
+    print "##### Basic Usage"
+    print ("Print the servers list: " + s[0] + " " + "-pl")
+    print ("Account search: " + s[0] + " " + "http://site.server")
+
 if len(s) == 1:
     usage()
     exit()
-if s[1] == '-h':
+if s[1] == "-h":
     usage()
     exit()
-if s[1] == '-pl':
+if s[1] == "-pl":
     print_link()
-    exit() 
+    exit()
 
 search_account(s[1])
