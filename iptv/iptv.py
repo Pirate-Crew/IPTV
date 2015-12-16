a8c262a2adae575110b5b0de7485cabce2cb53ba
Ludo237 <ludovico1990@hotmail.it>
2015-12-16 13:53:37 +0100

    Improved menu
    Added new options
    Fixed some weird printing

--- iptv/iptv.py
+++ iptv/iptv.py
@@ -15,28 +15,33 @@ def menu():
     print colored.yellow("################")
     print ""
     print colored.blue("Menu")
+    print "0 - Exit"
     print "1 - Search for some Servers"
-    print "2 - Select language, default is Italian"
-    print "3 - Brute force random accounts"
-    print "4 - Exit"
+    print "2 - Look at the servers list"
+    print "3 - Select language, default is Italian"
+    print "4 - Brute force random accounts"
     print ""
 
 while True:
     menu()
     choosenMenu = int(raw_input("Please select an option: "))
-    if choosenMenu == 1:
+    if choosenMenu == 0:
+        print colored.red("Bye bye")
+        break;
+    elif choosenMenu == 1:
         print colored.green("Fetching URLs plase wait...")
         cr.search_links()
         print colored.green("Done, 30 URLs founded")
     elif choosenMenu == 2:
+        print colored.green("Printing server list")
+        for server in cr.parsedUrls:
+            print server
+    elif choosenMenu == 3:
         language = str(raw_input("What language do you need? (it, en, es): "))
         cr.change_language(language)
         print colored.green("Language changed")
-    elif choosenMenu == 3:
+    elif choosenMenu == 4:
         result =  cr.search_accounts()
         print colored.green(result)
-    elif choosenMenu == 4:
-        print colored.red("Bye bye")
-        break
     else:
         print colored.red("Option not recognized")
