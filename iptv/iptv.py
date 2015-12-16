fedc930f981b13ba24f0559a77fe7fc9f7126d03
Ludo237 <ludovico1990@hotmail.it>
2015-12-16 12:37:28 +0100

    New CLI interface
    Removed useless license
    Refactored imports
    Removed old parameters
    Added a fancy new menu which will be permanent until the user decide to
    exit.
    Minor optimisations

--- iptv/iptv.py
+++ iptv/iptv.py
@@ -1,57 +1,42 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
-
-#########################################################################
-#  This program is free software; you can redistribute it and/or modify #
-#  it under the terms of the GNU General Public License as published by #
-#  the Free Software Foundation; either version 2 of the License, or    #
-#  (at your option) any later version.                                  #
-#                                                                       #
-#  This program is distributed in the hope that it will be useful,      #
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of       #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
-#  GNU General Public License for more details.                         #
-#                                                                       #
-#  You should have received a copy of the GNU General Public License    #
-#  along with this program; if not, write to the Free Software          #
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,           #
-#  MA 02110-1301, USA.                                                  #
-############################# DISCALIMER ################################
-#  Usage of this software for probing/attacking targets without prior   #
-#  mutual consent, is illegal. It's the end user's responsability to    #
-#  obey alla applicable local laws. Developers assume no liability and  #
-#  are not responible for any missue or damage caused by thi program    #
-#########################################################################
-
-#by Pinperepette - The Pirate
-
 import Crawler
 from clint.textui import colored
-from sys import argv as s
 
-#Parameters
-version = "v1.0"
-search = "Xtream Codes v1.0.59.5"
 cr = Crawler.Crawler("it")
 
+"""Print menu
+Easy menu for CLI navigation
+"""
 def menu():
+    print ""
     print colored.yellow("################")
-    print colored.yellow("##### ") + s[0] + colored.yellow(" ##")
-    print colored.yellow("##### ") + version + colored.yellow(" #####")
+    print colored.yellow("##### IPTV #####")
+    print colored.yellow("##### v1.0 #####")
     print colored.yellow("################")
     print ""
-    print colored.blue("Basic Usage")
-    print "Print the servers list: " + colored.red(s[0] + " " + "-pl")
-    print "Account search: " + colored.red(s[0] + " " + "http://site.server")
+    print colored.blue("Menu")
+    print "1 - Search for some Servers"
+    print "2 - Select language, default is Italian"
+    print "3 - Brute force random accounts"
+    print "4 - Exit"
+    print ""
 
-if len(s) == 1:
+while True:
     menu()
-    exit()
-elif s[1] == "-h":
-    menu()
-    exit()
-elif s[1] == "-pl":
-    cr.search_links()
-    exit()
-else:
-    cr.search_accounts(s[1])
+    choosenMenu = int(raw_input("Please select an option: "))
+    if choosenMenu == 1:
+        print colored.green("Fetching URLs plase wait...")
+        cr.search_links()
+        print colored.green("Done, 30 URLs founded")
+    elif choosenMenu == 2:
+        language = str(raw_input("What language do you need? (it, en, es): "))
+        cr.change_language(language)
+        print colored.green("Language changed")
+    elif choosenMenu == 3:
+        result =  cr.search_accounts()
+        print colored.green(result)
+    elif choosenMenu == 4:
+        print colored.red("Bye bye")
+        break
+    else:
+        print colored.red("Option not recognized")
