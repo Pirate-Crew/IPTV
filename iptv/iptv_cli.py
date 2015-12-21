060f69e222bc96e910a949ef209dc220d225acf3
Ludo237 <ludovico1990@hotmail.it>
2015-12-21 20:54:17 +0100

    Added ability to specify one URL to brute force

--- iptv/iptv_cli.py
+++ iptv/iptv_cli.py
@@ -22,7 +22,8 @@ def menu():
     print "1 - Search for some Servers"
     print "2 - Look at the servers list"
     print "3 - Select language, default is Italian"
-    print "4 - Brute force random accounts"
+    print "4 - Brute force random server"
+    print "5 - Brute force specific server"
     print ""
 
 while True:
@@ -37,8 +38,8 @@ while True:
         print colored.green("Done, 30 URLs founded")
     elif choosenMenu == 2:
         print colored.green("Printing server list")
-        for server in cr.parsedUrls:
-            print server
+        for index, server in enumerate(cr.parsedUrls):
+            print "[" + str(index) + "] - " + server
     elif choosenMenu == 3:
         language = str(raw_input("What language do you need? (it, en, es): "))
         cr.change_language(language)
@@ -46,5 +47,13 @@ while True:
     elif choosenMenu == 4:
         result =  cr.search_accounts()
         print colored.green(result)
+    elif choosenMenu == 5:
+        index = int(raw_input("Please provide the number near the URLs founded: "))
+        try:
+            url = cr.parsedUrls[index]
+            result = cr.search_accounts(url)
+            print colored.green(result)
+        except IndexError as e:
+            print colored.red("No URL founded at index: " + str(index)) 
     else:
         print colored.red("Option not recognized")
