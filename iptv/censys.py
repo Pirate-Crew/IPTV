5125d6d0319c375f89fdf319c94b13df821377c5
Pinperepette <pinperepette@gmail.com>
2016-01-07 23:47:00 +0100

    Create censys.py

---
+++ iptv/censys.py
@@ -0,0 +1,24 @@
+import sys
+import json
+import requests
+from termcolor import colored
+
+
+API_URL = ""
+UID = ""
+SECRET = ""
+
+def censys(self, page=1):
+
+	if API_URL == "":
+		segale_rosso = colored ('[*]','red')
+		print (segale_rosso + " require The Censys API, https://www.censys.io/api ")
+	else: 	
+		q = "Xtream Codes v1.0.59.5 Copyright 2014-2015"
+		params = {'query' : q, 'page' : page}
+
+		res = requests.post(API_URL + "/search/ipv4", json = params, auth=(UID, SECRET))
+		payload = res.json()
+
+		for r in payload['results']:
+			print ("http://" + r["ip"])
